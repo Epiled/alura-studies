3cb6b2b6b039efe5066396498b836efd14706fe7
Felipe De Andrade <felipedeandrademendonca@gmail.com>
2022-07-14 17:59:16 -0300

    Alura Studies - Início

---
+++ src/components/Lista/Lista.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Lista() {
+  return(
+    <aside>
+      <h2>Estudos do dia</h2>
+      <ul>
+        <li>
+          <h3>React</h3>
+          <span>01:00:00</span>
+        </li>
+        <li>
+          <h3>Webpack</h3>
+          <span>00:30:00</span>
+        </li>
+      </ul>
+    </aside>
+  )
+}
+
+export default Lista;

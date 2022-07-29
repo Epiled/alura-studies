cb7bb6edbb289b24b440f3df10c2d275f281bbb3
Felipe De Andrade <felipedeandrademendonca@gmail.com>
2022-07-29 12:55:22 -0300

    Refatorando componentes

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -2,23 +2,14 @@ import React, { useState } from 'react';
 import style from './Lista.module.scss';
 import Item from './Item';
 
-function Lista() {
-  const [tarefas, setTarefas] = useState([
-    {
-      tarefa: 'React',
-      tempo: '01:00:00',
-    },
-    {
-      tarefa: 'Webpack',
-      tempo: '00:30:00',
-    },
-  ])
+import { ITarefa } from '../../types/tarefa';
+
+
+function Lista({tarefas}: {tarefas: ITarefa[]}) {
 
   return (
     <aside className={style.listaTarefas}>
-      <h2 onClick={() => {
-        setTarefas([...tarefas, {tarefa: 'Estudo', tempo: '01:30:00'}])
-      }}>Estudos do dia</h2>
+      <h2>Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item
